0b4e1fcd897a34d8055251ef2bc9231bbc7d9ff5
tmxiong <990895247@qq.com>
2017-09-11 16:48:34 +0800

    u

--- config/urls.js
+++ config/urls.js
@@ -17,5 +17,5 @@ exports.getArticleDetail = function (docid) {
 // 福彩3d/高频彩/数字彩的链接
 exports.getPlayTips = function (type) {
     // type = fc/gpc/szc/csxw
-    return 'https://m.qmcai.com/zixun/detail.html?_id=19264&sourceFrom=zixunrevision&categoryId='+ type +'&newWebview=true&backH5Control=true&h5ControlTitle=true&clientLogin=true&version=5.2.16&channel=970'
+    return 'https://m.qmcai.com/support/cmsv2/information/queryContent?parameter=%7B%22command%22:%22queryContent%22,%22categoryId%22:%22'+ type +'%22,%22offset%22:0,%22size%22:15,%22platform%22:%22html%22,%22version%22:%225.2.16%22%7D&callback=jsonp5'
 };
