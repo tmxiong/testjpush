1a4dd5fe2cea89cf4d0ddfc22d294e49afd99fc9
tmxiong <990895247@qq.com>
2017-09-11 15:32:50 +0800

    config

--- config/lotterys.js
+++ config/lotterys.js
@@ -9,25 +9,25 @@ const data = [
         icon:require('../imgs/lotteryIcons/bjpk10.png'),
         name:'北京PK10',
         id:'bjpk10',
-        des:''
+        des:'超多比赛通宵玩'
     },
     {
         icon:require('../imgs/lotteryIcons/bjkl8.png'),
         name:'北京快乐8',
         id:'bjkl8',
-        des:''
+        des:'火热高频游戏'
     },
     {
         icon:require('../imgs/lotteryIcons/jsks.png'),
         name:'江苏快3',
         id:'jsks',
-        des:''
+        des:'玩色子轻松上手'
     },
     {
         icon:require('../imgs/lotteryIcons/hbk3.png'),
         name:'湖北快3',
         id:'hbk3',
-        des:''
+        des:'玩色子轻松上手'
     },
 
     // {
@@ -40,31 +40,31 @@ const data = [
         icon:require('../imgs/lotteryIcons/cqssc.png'),
         name:'重庆时时彩',
         id:'cqssc',
-        des:''
+        des:'单注赢十万'
     },
     {
         icon:require('../imgs/lotteryIcons/xjssc.png'),
         name:'新疆时时彩',
         id:'xjssc',
-        des:''
+        des:'单注赢十万'
     },
     {
         icon:require('../imgs/lotteryIcons/tjssc.png'),
         name:'天津时时彩',
         id:'tjssc',
-        des:''
+        des:'单注赢十万'
     },
     {
         icon:require('../imgs/lotteryIcons/ynssc.png'),
         name:'云南时时彩',
         id:'ynssc',
-        des:''
+        des:'单注赢十万'
     },
     {
         icon:require('../imgs/lotteryIcons/shssl.png'),
         name:'上海时时乐',
         id:'shssl',
-        des:''
+        des:'单注赢十万'
     },
 
     // {
@@ -82,14 +82,14 @@ const data = [
         icon:require('../imgs/lotteryIcons/fc3d.png'),
         name:'福彩3D',
         id:'fc3d',
-        des:''
+        des:'轻松赢千元'
     },
 
     {
         icon:require('../imgs/lotteryIcons/tcpl5.png'),
         name:'体彩排列5',
         id:'tcpl5',
-        des:''
+        des:'2元中10万'
     },
 
 
@@ -134,19 +134,19 @@ const data = [
         icon:require('../imgs/lotteryIcons/js11x5.png'),
         name:'江苏11选5',
         id:'js11x5',
-        des:''
+        des:'10分钟1期'
     },
     {
         icon:require('../imgs/lotteryIcons/ln11x5.png'),
         name:'辽宁11选5',
         id:'ll11x5',
-        des:''
+        des:'10分钟1期'
     },
     {
         icon:require('../imgs/lotteryIcons/gx11x5.png'),
         name:'广西11选5',
         id:'gx11x5',
-        des:''
+        des:'10分钟1期'
     },
     // {
     //     name:'黑龙江11选5',
@@ -157,31 +157,31 @@ const data = [
         icon:require('../imgs/lotteryIcons/sh11x5.png'),
         name:'上海11选5',
         id:'sh11x5',
-        des:''
+        des:'10分钟1期'
     },
     {
         icon:require('../imgs/lotteryIcons/gd11x5.png'),
         name:'广东11选5',
         id:'gd11x5',
-        des:''
+        des:'10分钟1期'
     },
     {
         icon:require('../imgs/lotteryIcons/sd11x5.png'),
         name:'山东11选5',
         id:'sd11x5',
-        des:''
+        des:'10分钟1期'
     },
     {
         icon:require('../imgs/lotteryIcons/jx11x5.png'),
         name:'江西11选5',
         id:'jx11x5',
-        des:''
+        des:'10分钟1期'
     },
     {
         icon:require('../imgs/lotteryIcons/ah11x5.png'),
         name:'安徽11选5',
         id:'af11x5',
-        des:''
+        des:'10分钟1期'
     },
 ];
 
