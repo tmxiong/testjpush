b3caa5ba6e106b83389f8e0d86312b834b1caefe
tmxiong <990895247@qq.com>
2017-09-11 17:39:19 +0800

    add urls

--- config/urls.js
+++ config/urls.js
@@ -19,3 +19,8 @@ exports.getPlayTips = function (type) {
     // type = fc/gpc/szc/csxw
     return 'https://m.qmcai.com/support/cmsv2/information/queryContent?parameter=%7B%22command%22:%22queryContent%22,%22categoryId%22:%22'+ type +'%22,%22offset%22:0,%22size%22:15,%22platform%22:%22html%22,%22version%22:%225.2.16%22%7D&callback=jsonp5'
 };
+
+// 福彩3d/高频彩/数字彩的链接详情
+exports.getPlayTipsDetail = function (id) {
+    return 'https://m.qmcai.com/zixun/detail.html?_id=' + id
+};
